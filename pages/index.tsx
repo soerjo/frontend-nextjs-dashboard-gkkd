257a52e032efc0ec8d8bebf3b03e364500d07062
soerjo <ryohastomo@gmail.com>
2022-08-21 17:09:16 +0700

    bikin branch

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,18 +8,7 @@ const Home: NextPage = () => {
     <>
       <main className="container px-8 mx-auto flex flex-wrap items-center justify-between bg-white">
         <div className="flex flex-wrap md:flex-nowrap  items-center w-full gap-2">
-          <div className=" w-full lg:basis-2/3">
-            <Banner />
-          </div>
-          <div className="drop-shadow-lg w-full lg:basis-1/3">
-            <News />
-          </div>
-        </div>
-
-        <div className="flex pt-2 p-2 w-full ">
-          <div className="w-full">
-            <EventList />
-          </div>
+          <h1>Judul web</h1>
         </div>
       </main>
     </>
